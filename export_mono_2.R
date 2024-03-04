4478043668cd902d65db33084164ce170c8000c1
María del Rosario Miranda <71475725+mdelrosariom@users.noreply.github.com>
2024-03-04 09:54:52 +0100

    Add files via upload

---
+++ export_mono_2.R
@@ -0,0 +1,30 @@
+library(openxlsx)
+
+export_mono_2 <- function(output_mono, tree_name) {
+  # Extract lists from the output
+  all_of_mono_list <- output_mono$all_of_mono
+  to_prune_list <- output_mono$to_prune
+  lost_case_ask_ben_list <- output_mono$lost_case_ask_ben
+  still_hope_list <- output_mono$still_hope
+  
+  # Combine lists into data frames
+  df_all_of_mono <- as.data.frame(do.call(rbind, all_of_mono_list))
+  df_to_prune <- as.data.frame(do.call(rbind, to_prune_list))
+  df_lost_case_ask_ben <- as.data.frame(do.call(rbind, lost_case_ask_ben_list))
+  df_still_hope <- as.data.frame(do.call(rbind, still_hope_list))
+  
+  # Create the name of the file
+  file_name <- paste("C:/Users/mdrmi/OneDrive/Escritorio/info_about_mono/", tree_name, "_monophyletic_info.xlsx", sep = "")
+  
+  # Export to Excel
+  write.xlsx(
+    list(
+      'all_monophyletic' = df_all_of_mono,
+      'to_prune' = df_to_prune,
+      'ask_ben' = df_lost_case_ask_ben,
+      'still_hope' = df_still_hope
+    ),
+    file = file_name
+  )
+}
+
