6393bcd68877e0ed079a3ee2cee6b49dcda58c57
María del Rosario Miranda <71475725+mdelrosariom@users.noreply.github.com>
2024-03-11 11:16:24 +0100

    Add files via upload

--- export_mono_finalish_version.R
+++ export_mono_finalish_version.R
@@ -1,54 +1,65 @@
-
 library(openxlsx)
-export_mono_4 <- function(output_tree_check_mono, tree_name){
 
-a <- output_tree_check_mono$all_of_mono
-pad <-max(sapply(a, length))
-
-for (i in 1:length(a)){
-  length(a[[i]]) <- pad
-}
-a <- data.frame(t(do.call(rbind, a)), stringsAsFactors = FALSE)
-
-
-
-b <- output_tree_check_mono$to_prune
-pad1 <- max(sapply(b, length))
-
-for (j in 1:length(b)){
-  length(b[[j]]) <- pad1
+export_mono_4 <- function(output_tree_check_mono, tree_name) {
   
-}
-b <- data.frame(t(do.call(rbind, b)), stringsAsFactors = FALSE)
-
-c <- output_tree_check_mono$lost_case_ask_ben
-pad2 <- max(sapply(c, length))
-for (k in 1:length(c)){
-  length(c[[k]]) <- pad2
+  a <- output_tree_check_mono$all_of_mono
+  if (!is.null(unlist(a))) {
+    pad <- max(sapply(a, length))
+    
+    for (i in 1:length(a)) {
+      length(a[[i]]) <- pad
+    }
+    a <- data.frame(t(do.call(rbind, a)), stringsAsFactors = FALSE)
+  } else {
+    a <- list()
+  }
   
-}
-c <- data.frame(t(do.call(rbind, c)), stringsAsFactors = FALSE)
-
-d <- output_tree_check_mono$still_hope
-pad3 <- max(sapply(d,length))
-for (l in 1:length(d)){
-  length(d[[l]]) <- pad3
+  b <- output_tree_check_mono$to_prune
+  if (!is.null(unlist(b))) {
+    pad1 <- max(sapply(b, length))
+    
+    for (j in 1:length(b)) {
+      length(b[[j]]) <- pad1
+    }
+    b <- data.frame(t(do.call(rbind, b)), stringsAsFactors = FALSE)
+  } else {
+    b <- list()
+  }
   
+  c <- output_tree_check_mono$lost_case_ask_ben
+  if (!is.null(unlist(c))) {
+    pad2 <- max(sapply(c, length))
+    for (k in 1:length(c)) {
+      length(c[[k]]) <- pad2
+    }
+    c <- data.frame(t(do.call(rbind, c)), stringsAsFactors = FALSE)
+  } else {
+    c <- list()
+  }
+  
+  d <- output_tree_check_mono$still_hope
+  if (!is.null(unlist(d))) {
+    pad3 <- max(sapply(d, length))
+    for (l in 1:length(d)) {
+      length(d[[l]]) <- pad3
+    }
+    d <- data.frame(t(do.call(rbind, d)), stringsAsFactors = FALSE)
+  } else {
+    d <- list()
+  }
+  
+  file_name <- paste("C:/Users/mdrmi/OneDrive/Escritorio/info_about_mono_2/", tree_name, "_monophyletic_info_2.xlsx", sep = "")
+  
+  # Export to Excel
+  write.xlsx(
+    list(
+      'all_monophyletic' = a,
+      'to_prune' = b,
+      'ask_ben' = c,
+      'still_hope' = d
+    ),
+    file = file_name
+  )
 }
-d <- data.frame(t(do.call(rbind, d)), stringsAsFactors = FALSE)
 
-
-file_name <- paste("C:/Users/mdrmi/OneDrive/Escritorio/info_about_mono_2/","ok", tree_name , "_monophyletic_info.xlsx", sep = "")
-
-# Export to Excel
-write.xlsx(
-  list(
-    'all_monophyletic' =a,
-    'to_prune' = b,
-    'ask_ben' = c,
-    'still_hope' = d
-  ),
-  file = "chupapija.xlsx"
-)
-}
 
